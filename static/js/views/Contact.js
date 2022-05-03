16cd68b057d3592c05eb23f680b5567d16667359
Awe42 <anish.pathak5@gmail.com>
2022-05-04 01:21:33 +0530

    minor adjustments

--- static/js/views/Contact.js
+++ static/js/views/Contact.js
@@ -13,8 +13,8 @@ export default class extends AbstractView {
             Please write to us your queries or suggestions to the following email ID. Ideally, you should be getting a response within two days, if not, please feel free to write a short reminder.
             </p>
             <p>
+                <span class="fa fa-fw fa-envelope-o"></span>
                 <a href="mailto:amygdalaai@gmail.com" class="link">
-                    <span class="fa fa-fw fa-envelope-o"></span>
                     amygdalaai@gmail.com
                 </a>
             </p>
